de2caa2a0826480891c1f4c4c03146a798177c52
Tristan Slater <hello@trslater.ca>
2022-08-24 11:01:03 -0700

    Add per-pixel manipulation and init seed points

--- src/index.js
+++ src/index.js
@@ -15,3 +15,23 @@ canvas.height = height
 viewport.appendChild(canvas)
 
 const context = canvas.getContext("2d")
+const imageData = context.createImageData(width, height)
+const colorValues = imageData.data
+
+const setPixel = (i, j, [r, g, b, a]) => {
+    colorValues[i*width*4 + j*4 + 0] = r
+    colorValues[i*width*4 + j*4 + 1] = g
+    colorValues[i*width*4 + j*4 + 2] = b
+    colorValues[i*width*4 + j*4 + 3] = a
+}
+
+const numSeeds = 5
+
+for (let k = 0; k < numSeeds; ++k) {
+    const i = Math.floor(Math.random()*width)
+    const j = Math.floor(Math.random()*width)
+
+    setPixel(i, j, [0, 0, 0, 255])
+}
+
+context.putImageData(imageData, 0, 0)
