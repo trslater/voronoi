f562e6806ad7e77e309c4f32c49101c7580d398f
Tristan Slater <hello@trslater.ca>
2022-08-24 11:00:18 -0700

    Pivot again to straight canvas

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import { GUI } from "dat.gui"
 
-import { makeApp } from "./app.js"
-
 const gui = new GUI()
-const app = makeApp(document.getElementById("voronoi"))
+
+const viewport = document.getElementById("viewport")
+
+const width = viewport.offsetWidth
+const height = viewport.offsetHeight
+
+const canvas = document.createElement("canvas")
+
+canvas.width = width
+canvas.height = height
+
+viewport.appendChild(canvas)
+
+const context = canvas.getContext("2d")
