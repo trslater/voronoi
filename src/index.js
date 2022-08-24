bec75598fe572ea415d15522054b3b64bab541d2
Tristan Slater <hello@trslater.ca>
2022-08-24 11:12:40 -0700

    Colorize seeds and store colors and origins

--- src/index.js
+++ src/index.js
@@ -20,20 +20,30 @@ const context = canvas.getContext("2d")
 const imageData = context.createImageData(width, height)
 const colorValues = imageData.data
 
-const setPixel = (i, j, [r, g, b, a]) => {
-    colorValues[i*width*4 + j*4 + 0] = r
-    colorValues[i*width*4 + j*4 + 1] = g
-    colorValues[i*width*4 + j*4 + 2] = b
-    colorValues[i*width*4 + j*4 + 3] = a
+const setPixel = ([x, y], [r, g, b, a]) => {
+    colorValues[y*width*4 + x*4 + 0] = r
+    colorValues[y*width*4 + x*4 + 1] = g
+    colorValues[y*width*4 + x*4 + 2] = b
+    colorValues[y*width*4 + x*4 + 3] = a
 }
 
 const numSeeds = 5
+const seedOrigins = []
+const seedColors = []
 
 for (let k = 0; k < numSeeds; ++k) {
-    const i = Math.floor(Math.random()*width)
-    const j = Math.floor(Math.random()*width)
+    seedOrigins[k] = [
+        Math.floor(Math.random()*width),
+        Math.floor(Math.random()*height)
+    ]
+    seedColors[k] = [
+        Math.floor(Math.random()*255),
+        Math.floor(Math.random()*255),
+        Math.floor(Math.random()*255),
+        255
+    ]
 
-    setPixel(i, j, [0, 0, 0, 255])
+    setPixel(seedOrigins[k], seedColors[k])
 }
 
 context.putImageData(imageData, 0, 0)
